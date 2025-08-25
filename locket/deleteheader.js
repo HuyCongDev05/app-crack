e9a8708e13d2bf2d49f288292cc86f9edf33023c
HuyCongDev05 <congtuoitre1007@gmail.com>
2025-08-25 15:45:44 +0700

    change

--- locket/deleteheader.js
+++ locket/deleteheader.js
@@ -1,20 +1,2 @@
 const version = 'V1.0.2';
-
-function setHeaderValue(headers, key, value) {
-    const lowerKey = key.toLowerCase();
-    if (lowerKey in headers) {
-        headers[lowerKey] = value;
-    } else {
-        headers[key] = value;
-    }
-}
-
-var modifiedHeaders = $request.headers;
-
-setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", "");
-
-setHeaderValue(modifiedHeaders, "X-Device-Country", "US");
-setHeaderValue(modifiedHeaders, "X-Subscriber-Country", "US");
-setHeaderValue(modifiedHeaders, "X-Apple-Locale", "en_US");
-
-$done({headers: modifiedHeaders});
+function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
