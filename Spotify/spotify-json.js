470b9682082b6b25ce2144f900133b4f4a7b9075
Nguyễn Huy Công <congtuoitre1007@gmail.com>
2026-06-25 08:51:30 +0700

    first commit

---
+++ Spotify/spotify-json.js
@@ -0,0 +1,13 @@
+console.log(`spotify-json-2025.06.20`);
+let url = $request.url;
+if (url.includes('com:443')) {
+    url = url.replace(/com:443/, 'com');
+}
+if (url.includes('platform=iphone')) {
+    url = url.replace(/platform=iphone/, 'platform=ipad');
+} else {
+    console.log('Không cần xử lý');
+}
+$done({
+    url
+});
